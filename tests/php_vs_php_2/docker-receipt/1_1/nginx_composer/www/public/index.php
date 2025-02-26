f2ea967caf9974b51af1f685ee11b6ca89697e68
Danilo Silva <contact@danilocgsilva.me>
2025-02-26 12:49:56 -0300

    Added nginx with composer

---
+++ tests/php_vs_php_2/docker-receipt/1_1/nginx_composer/www/public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use AVsN\ApacheComposerNginx\ComposerHtmlNginx;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+print(ComposerHtmlNginx::helloWorld());
---
+++ tests/php_vs_php_2/docker-receipt/1_1/nginx_composer/www/src/ComposerHtmlNginx.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AVsN\ApacheComposerNginx;
+
+class ComposerHtmlNginx
+{
+    public static function helloWorld(): string
+    {
+        return "Hello world! I was served by a simples possible composer application! With NGginx!";
+    }
+}
